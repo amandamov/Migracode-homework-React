8defd0fd552a8fbd32a30bb82829770cba5591c7
Amanda Maria <amanda.moov@gmail.com>
2023-01-03 11:03:35 +0100

    exercise 20 - display a Customer profile

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import moment from "moment";
+import SearchButton from "./SearchButton";
 
 const Row = props => {
   const [clicked, setClicked] = useState(false);
@@ -22,6 +23,12 @@ const Row = props => {
       <td>{props.booking.checkInDate}</td>
       <td>{props.booking.checkOutDate}</td>
       <td>{numberNights}</td>
+      <td>
+        <SearchButton
+          onClick={props.changeCustomerId(props.booking.id)}
+          content={"Customer Profile"}
+        />
+      </td>
     </tr>
   );
 };
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 import Row from "./Row";
+import CustomerProfile from "./CustomerProfile";
 
 const SearchResults = props => {
+  const [customerId, setCustomerId] = useState(0);
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Title</th>
-          <th scope="col">First name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room id</th>
-          <th scope="col">Check in date</th>
-          <th scope="col">Check out date</th>
-          <th scope="col">Number of nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map(booking => {
-          return <Row key={booking.id} booking={booking} />;
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">First name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room id</th>
+            <th scope="col">Check in date</th>
+            <th scope="col">Check out date</th>
+            <th scope="col">Number of nights</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.results.map(booking => {
+            return (
+              <Row
+                key={booking.id}
+                changeCustomerId={setCustomerId}
+                booking={booking}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={customerId} />
+    </div>
   );
 };
 
